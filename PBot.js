30421bb64c819cda0d4e255adc87f96a6ec26c84
nosremmaS <saememy24@gmail.com>
2020-01-14 13:57:12 -0500

    Actually made the bot work

--- PBot.js
+++ PBot.js
@@ -1,26 +1,29 @@
 const Discord = require('discord.js');
 const {
-        prefix,
-        token,
+        prefix, token,
 } = require('./config.json');
-const ballResponses = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "Signs point to yes.", "Yes.", "Outlook good.", "Most Likely.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+const {
+	help
+} = require('./longcommands.json');
 const client = new Discord.Client();
+const ballResponses = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "Signs point to yes.", "Yes.", "Outlook good.", "Most Likely.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+
 
 client.once('ready', () => {
-    console.log('Ready to operate!');
+        console.log('Ready to operate!');
 });
 
 client.once('reconnecting', () => {
-    console.log('Reconnecting...');
-    });
-
-client.once('disconnect', () =>
-    console.log('Disconnected, attempting to reconnect...');
+        console.log('Reconnecting!');
 });
 
-client.on('ready, () => {
-    client.user.setActivity('For commands', { type: 'LISTENING' });
+client.once('disconnect', () => {
+        console.log('Disconnect!');
 });
+
+client.on('ready', () => {
+        client.user.setActivity('For Commands', { type: 'WATCHING' });
+        });
 
 // Constants and logging completed. Entering core bot
 
@@ -28,15 +31,14 @@ client.on('message', async message => {
     if (!message.content.startsWith(prefix)) return;
     if (message.author.bot) return;
     // Validating user and messages
-    if (message.content === `${prefix}ping)) {
+    if (message.content === `${prefix}ping`) {
         message.channel.send(`pong!`)
-        message.channel.react(`checkered_flag`);
         }
     //"Ping-pong" message for testing if bot is online and ready
 
     //Setting up the "8-Ball" command
 
-    else if (message.content.startsWith(`?8ball`)) {
+    else if (message.content.startsWith(`${prefix}8ball`)) {
     message.channel.send(ballResponses [Math.floor(Math.random()*ballResponses .length )])
     }
     //That's the entire 8-ball command
@@ -45,4 +47,12 @@ client.on('message', async message => {
 		message.reply(message.author.avatarURL);
 	}
 
+	else if (message.content === `${prefix}help`) {
+		message.channel.send(`${help}`)
+	}
+	else (message.channel.send(`It doesn't look like that's one of my commands! Please try another command.`))
+	
+})
+
+client.login(token);
 
